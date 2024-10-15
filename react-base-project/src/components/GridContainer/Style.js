5369086d9e5b8800a9bda1286c1b85840b9b8be2
LE Monoplayer <jpedrossantana@gmail.com>
2024-10-15 18:05:03 -0300

    Deixando os quadrados um do lado do outro

--- react-base-project/src/components/GridContainer/Style.js
+++ react-base-project/src/components/GridContainer/Style.js
@@ -1,13 +1,14 @@
 import styled from "styled-components"
 
 const Container = styled.div`
-    display:flex;
-    flex-direction: row;
+    display: inline-block;
+    grid-template-columns: 2fr 2fr;
+    justify-content: center;
+    align-items: center;
     height: 350px;
     width: 500px;
     background-color: blue;
     margin: 10px;
-align-items: center
 `
 
 export { Container }
