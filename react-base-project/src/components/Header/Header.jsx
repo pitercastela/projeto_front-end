cbc4f09367cef9e3111a84e40b0ae93d0c02ae57
LE Monoplayer <jpedrossantana@gmail.com>
2024-10-20 13:46:25 -0300

    botão de tradução e logo do ibmec

--- react-base-project/src/components/Header/Header.jsx
+++ react-base-project/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Top } from "./Style";
 
 const Header = () => (
   <Top>
+    
     <div id="barra">Filtrar por: 
         <button>Ferramentas</button>
         <button>Tecnologias</button><button>Curso</button>
