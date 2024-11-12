ec33f75d32fa8de0df2a46e9d805cd99fcaca002
Lucas Alves <lucasacastela@gmail.com>
2024-11-12 16:52:17 -0300

    ajeitando a logo

--- react-base-project/src/components/Head/Head.jsx
+++ react-base-project/src/components/Head/Head.jsx
@@ -5,19 +5,19 @@ const Head = (props) => (
   <Top>
 
     <a href="https://www.ibmec.br" target="ibmec" >
-    <img id="logoibmec" src="/imagens/logo-ibmec.jpg" alt="Logo IBMEC" style={{ cursor: 'pointer' }} />
+    <img id="logoibmec" src={`${process.env.PUBLIC_URL}/imagens/logo-ibmec.jpg`} alt="Logo IBMEC" style={{ cursor: 'pointer' }} />
     </a>
     <input id="pesquisar" placeholder="Pesquisa"/>
     <select id='select-tradu'>
         <option value="">Português</option>
         <option value= "Inglês">Inglês</option>
     </select>
-    
+
     </Top>
     <div className="container">
       {props.children}
     </div>
-    
+
     </>
 )
 
